e094d6647b4d674d560ad990539a9307c5c198af
Karim <m.karimharras@gmail.com>
2021-03-17 09:31:06 +0100

    back end fonctionnel

---
+++ src/main/java/com/lov2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.lov2code.ecommerce.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.lov2code.ecommerce.entity.Product;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product,Long>{
+
+}
