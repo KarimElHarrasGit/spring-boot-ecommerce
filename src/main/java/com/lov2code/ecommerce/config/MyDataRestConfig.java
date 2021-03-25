5b07b54a650c50ea606b1760a1484c6358683881
Karim <m.karimharras@gmail.com>
2021-03-25 10:17:23 +0100

    Exposition des ids des entites product category

--- src/main/java/com/lov2code/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/lov2code/ecommerce/config/MyDataRestConfig.java
@@ -1,5 +1,13 @@
 package com.lov2code.ecommerce.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -11,11 +19,18 @@ import com.lov2code.ecommerce.entity.ProductCategory;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
+	private EntityManager entityManager;
+
+	@Autowired
+	public MyDataRestConfig(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-		
+
 		HttpMethod[] theUnsupportedActions = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE };
-		
+
 		// disable http methods for Product : put, post, delete
 		config.getExposureConfiguration().forDomainType(Product.class)
 				.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
@@ -25,6 +40,20 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 		config.getExposureConfiguration().forDomainType(ProductCategory.class)
 				.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
 				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+		exposeIds(config);
 	}
 
+	private void exposeIds(RepositoryRestConfiguration config) {
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+		List<Class> entityClasses = new ArrayList<>();
+
+		for (EntityType tempEntityType : entities) {
+			entityClasses.add(tempEntityType.getJavaType());
+		}
+
+		Class[] domainTypes = entityClasses.toArray(new Class[0]);
+		config.exposeIdsFor(domainTypes);
+	}
 }
